401fe0f5bf4eb46b8f96a36c05296c75fa98929f
Mark Howison <mhowison@ripl.org>
2021-04-13 13:04:29 -0400

    added ensemble coefficient output

--- bolasso-selection.py
+++ bolasso-selection.py
@@ -3,24 +3,31 @@ import sys
 
 # Parse command-line arguments
 threshold  = float(sys.argv[1])
-coef_files = sys.argv[2:-2]
+coef_files = sys.argv[2:-3]
+avg_file   = sys.argv[-3]
 freq_file  = sys.argv[-2]
 out_file   = sys.argv[-1]
 
 # Load coefficients and calculate non-zero frequency
 var = pd.read_csv(coef_files[0], index_col="var")
-var.drop("intercept", inplace=True)
 var["freq"] = (var.coef != 0).astype(int)
 for coef_file in coef_files[1:]:
-    var["freq"] += (pd.read_csv(coef_file, index_col="var").coef != 0).astype(int)
+    var1 = pd.read_csv(coef_file, index_col="var")
+    var["coef"] += var1.coef
+    var["freq"] += (var1.coef != 0).astype(int)
+var["coef"] /= len(coef_files)
 var["freq"] /= len(coef_files)
+
+# Write average coefficients to file
+var[var.coef != 0].to_csv(avg_file)
 
 # Write frequencies to file
 del var["coef"]
+var.drop("intercept", inplace=True)
 var.to_csv(freq_file)
 
 # Select variables at >= threshold
-selected = set(var[var["freq"] >= threshold].index)
+selected = set(var[var.freq >= threshold].index)
 
 # Ensure that base variables are preserved for any selected interaction
 for name in list(selected):
